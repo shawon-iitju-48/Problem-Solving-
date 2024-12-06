06461f41e9fcf8011601e4baab4a558b20508b03
Md. Bokhtiar Nadeem Shawon <shawon.iitju.48@gmail.com>
2024-12-07 03:14:46 +0600

    1095 solved

--- Leetcode/1095.find-in-mountain-array.cpp
+++ Leetcode/1095.find-in-mountain-array.cpp
@@ -15,11 +15,50 @@
  * };
  */
 
-class Solution {
+class Solution
+{
 public:
-    int findInMountainArray(int target, MountainArray &mountainArr) {
-        
+    int findInMountainArray(int target, MountainArray &mountainArr)
+    {
+        int f = 0, l = mountainArr.length() - 1, mid, topPoint;
+        while (f < l)
+        {
+            mid = f + (l - f) / 2;
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1))
+                f = mid + 1;
+            else
+                l = mid;
+        }
+        topPoint = f;
+
+        f = 0, l = topPoint;
+        while (f <= l)
+        {
+            mid = f + (l - f) / 2;
+            int x = mountainArr.get(mid);
+
+            if (x == target)
+                return mid;
+            if (x < target)
+                f = mid + 1;
+            else
+                l = mid - 1;
+        }
+
+        f = topPoint + 1, l = mountainArr.length() - 1;
+        while (f <= l)
+        {
+            mid = f + (l - f) / 2;
+            int x = mountainArr.get(mid);
+
+            if (x == target)
+                return mid;
+            if (x < target)
+                l = mid - 1;
+            else
+                f = mid + 1;
+        }
+        return -1;
     }
 };
 // @lc code=end
-
